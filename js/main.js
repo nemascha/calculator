4dd57aeac4c331389d8c7e8ff3ac55e23d30c82a
nemascha <nemascha@gmail.com>
2017-07-29 21:52:15 +0300

    [ADD] js switch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+	var mainButton = document.querySelector('.main-button');
+	mainButton.addEventListener('click', function(){
+			
+	switch(event.target.dataset['type']) {
+	
+	case 'number':
+		addNumber(event.target.dataset['value']);
+		break;
+	case 'divide':
+		break;
+	case 'multiply':
+		break;
+	case 'minus':
+		break;
+	case 'plus':
+		break;
+	case 'equal':
+		break;
+	}
+
+	
+})
+
 });
 
 
