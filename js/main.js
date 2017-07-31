34e51e24eefba1fa84a4d287edc27115ad371ea2
mbury@scnsoft.com <mbury@scnsoft.com>
2017-07-31 10:23:53 +0300

    update code structure

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	var mainButton = document.querySelector('.main-button');
 	mainButton.addEventListener('click', function(){
 			
-	switch(event.target.dataset['type']) {
-	
-	case 'number':
-		addNumber(event.target.dataset['value']);
-		break;
-	case 'divide':
-		break;
-	case 'multiply':
-		break;
-	case 'minus':
-		break;
-	case 'plus':
-		break;
-	case 'equal':
-		break;
-	}
+        switch(event.target.dataset['type']) {
+
+            case 'number':
+                addNumber(event.target.dataset['value']);
+                break;
+            case 'divide':
+                break;
+            case 'multiply':
+                break;
+            case 'minus':
+                break;
+            case 'plus':
+                break;
+            case 'equal':
+                break;
+        }
 
 	
 })
